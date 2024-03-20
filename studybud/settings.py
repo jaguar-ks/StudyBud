f556366833ab60f3537ce535a583c43e0379f291
jaguar-ks <fahdamineksouss@gmail.com>
2024-03-20 15:40:39 +0000

    PROGRESS : Working on static files such as .css files and images already configured the setting

--- studybud/settings.py
+++ studybud/settings.py
@@ -56,7 +56,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'templates'
+            BASE_DIR / 'templates',
+            BASE_DIR / 'static',
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -118,8 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
