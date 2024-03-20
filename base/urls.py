7ccd130253e81452bdb215b094fa08c7e5f10f84
jaguar-ks <fahdamineksouss@gmail.com>
2024-03-20 13:48:43 +0000

    PROGRESS : Added a new template for the user profile

--- base/urls.py
+++ base/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('register/', views.registerPage, name="register"),
     path('logout/', views.logoutUser, name="logout"),
     path('delete-msg/<str:pk>/', views.deleteMessage, name='delete-msg'),
+    path('user-profile/<str:pk>/', views.userProfile, name='user-profile'),
 ]
